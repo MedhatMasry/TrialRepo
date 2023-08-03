f78de7c64ab577646f4ee882862a68402f8e5cde
Medhat Masry <105849363+MedhatMasry@users.noreply.github.com>
2023-08-03 18:53:35 +0300

    Update Extract_Data_From_Json.py

--- Python_Scripts/Extract_Data_From_Json.py
+++ Python_Scripts/Extract_Data_From_Json.py
@@ -12,31 +12,8 @@ def get_campaign_name_and_ids(path):
     # for simplicity I will consider that the test file already downloaded in specific path.
     with open(path, 'r',encoding='utf-8') as test_file:
       data = test_file.read()
-    # data=requests.get(' url of "test_set.json" ').json()
-
-    # load json file
-    content = json.loads(data)
-
-    # create a list of dictionaries that contains only the test_sets
-    # test_set_list = content["testCaseSet"]
-
-    # #create empty list to hold the test 
-    # test_id_list=list()
-
-    # # iterate through the list and only look at test_ids
-    # for test_set in test_set_list:
-       # test_id_list.append(test_set["id"])
-
-    #for testing, print the test case ids         
-    #print(f'Test-Case-Set-List of "{path}"= {[int(i) for i in content["executionList"]]}')
-     #for testing, print the test case ids         
-    #print(f'Campaign_Name= {content["name"]}')
-    
-
-    #print(f'::set-output name=value1::{content["name"]}')
-    #print(f'::set-output name=value2::{content["executionList"]}')
-
     set_output('value1',content["name"])
+    set_output('value2',content["executionList"])
     
     return content["name"],content["executionList"]
     
